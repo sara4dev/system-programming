ed524f73848c065ce94e6154ecb11b95ec63289e
Saravana Kumar <saravana.kumar@meygam.io>
2016-04-07 23:44:33 -0500

    Updated readme & unit test

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# system-programming ![build status](https://travis-ci.org/saravanakumar-periyasamy/system-programming.svg?branch=master)
-system-programming
+# system-programming [<img src="https://travis-ci.org/saravanakumar-periyasamy/system-programming.svg?branch=master">](https://travis-ci.org/saravanakumar-periyasamy/system-programming)
+
+## Usage
+
+```diskusage.py <<directory/mount point>>```
+
+```eg., diskusage.py /tmp```
+   
+## Unit test
+   
+Below command executes the unit tests in `test_diskusage` module.
+
+```python -m unittest test_diskusage```
+   
--- test_diskusage.py
+++ test_diskusage.py
@@ -2,14 +2,15 @@ __author__ = 'saravanakumar'
 
 import unittest
 import diskusage
+import json
 
 class DiskUsageTest(unittest.TestCase):
     def test_list_files(self):
         mockOsWrapper = MockOsWrapper()
         d = diskusage.DiskUsage(mockOsWrapper)
-        diskusage_json = d.list_files('/test/')
-        print(diskusage_json)
-        self.assertEqual(1,1)
+        diskusage_dict = json.loads(d.list_files('/test/'))
+        self.assertEqual(25, diskusage_dict['files'][0]['size'])
+        self.assertEqual(3, len(diskusage_dict['files']))
 
 class MockOsWrapper:
     def dirWalk(self, path):
